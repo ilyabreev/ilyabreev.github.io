38d83c22473d8ea7f88a5b38367facac67e752df
Ilya Breev <ilyabreev@gmail.com>
2015-12-13 21:33:41 +0300

    подсказка для шара

--- 2016/app.js
+++ 2016/app.js
@@ -5,6 +5,10 @@ newYearApp.config(function($routeProvider) {
         .when('/', {
             templateUrl : 'pages/home.html',
             controller  : 'mainController'
+        })
+        .when('/pages/bb7ad48dc82a49588e7c34239fdeb8a8.html', {
+            templateUrl : 'pages/bb7ad48dc82a49588e7c34239fdeb8a8.html',
+            controller : 'mainController'
         });
 });
 
@@ -16,3 +20,16 @@ newYearApp.controller('mainController', function($scope) {
     $scope.answered = $scope.source == target;
   };
 });
+
+/*
+bb7ad48dc82a49588e7c34239fdeb8a8
+654067e916584fb2bb3a091299b80315
+aba7a7eb4d15476f8c4c592069597411
+b93482d68e3e4355aa9fc845aaabc9e8
+1f5c757a61da444f9a5f47bbab11c5f7
+1ec0a6051d0d4c03954fb3a57fb1ab3c
+91fa7ba44b0b4a44b7889b862ea6cac0
+5b9bdd73b1c341c7ab952bfe569ae4a3
+2bc0346ef50f40a681c98becf2f2b060
+b7911693d6cf467e8173d24045948aef
+*/
