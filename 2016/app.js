12956a4dc11a7d82be8812b28d90480ad8fae984
ilyabreev <ilyabreev@gmail.com>
2015-12-14 10:23:41 +0300

    Несколько qr-кодов и очередная страничка

--- 2016/app.js
+++ 2016/app.js
@@ -1,15 +1,31 @@
 var newYearApp = angular.module('newYearApp', ['ngRoute']);
 
 newYearApp.config(function($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl : 'pages/home.html',
-            controller  : 'mainController'
-        })
-        .when('/pages/bb7ad48dc82a49588e7c34239fdeb8a8.html', {
-            templateUrl : 'pages/bb7ad48dc82a49588e7c34239fdeb8a8.html',
-            controller : 'mainController'
+    var pages = [ 
+        'bb7ad48dc82a49588e7c34239fdeb8a8', // календарь
+        '654067e916584fb2bb3a091299b80315', // шар
+        'aba7a7eb4d15476f8c4c592069597411', // 
+        'b93482d68e3e4355aa9fc845aaabc9e8',
+        '1f5c757a61da444f9a5f47bbab11c5f7',
+        '1ec0a6051d0d4c03954fb3a57fb1ab3c',
+        '91fa7ba44b0b4a44b7889b862ea6cac0',
+        '5b9bdd73b1c341c7ab952bfe569ae4a3',
+        '2bc0346ef50f40a681c98becf2f2b060',
+        'b7911693d6cf467e8173d24045948aef'
+    ];
+    $routeProvider.when('/', {
+        templateUrl : 'pages/home.html',
+        controller  : 'mainController'
+    });
+
+    for (var i = 0; i < pages.length; i++) {
+        var page = pages[i];
+        var path = 'pages/' + page + '.html';
+        $routeProvider.when('/' + path, {
+            templateUrl : path,
+            controller: 'mainController'
         });
+    }
 });
 
 newYearApp.controller('mainController', function($scope) {
@@ -20,16 +36,3 @@ newYearApp.controller('mainController', function($scope) {
     $scope.answered = $scope.source == target;
   };
 });
-
-/*
-bb7ad48dc82a49588e7c34239fdeb8a8
-654067e916584fb2bb3a091299b80315
-aba7a7eb4d15476f8c4c592069597411
-b93482d68e3e4355aa9fc845aaabc9e8
-1f5c757a61da444f9a5f47bbab11c5f7
-1ec0a6051d0d4c03954fb3a57fb1ab3c
-91fa7ba44b0b4a44b7889b862ea6cac0
-5b9bdd73b1c341c7ab952bfe569ae4a3
-2bc0346ef50f40a681c98becf2f2b060
-b7911693d6cf467e8173d24045948aef
-*/
