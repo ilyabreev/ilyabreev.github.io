7d24b7888c3189a44ab2c1b0aa5883cf54e549be
Ilya Breev <ilyabreev@gmail.com>
2015-12-31 14:19:06 +0300

    доделка новогоднего квеста

--- 2016/app.js
+++ 2016/app.js
@@ -2,16 +2,8 @@ var newYearApp = angular.module('newYearApp', ['ngRoute']);
 
 newYearApp.config(function($routeProvider) {
     var pages = [ 
-        'bb7ad48dc82a49588e7c34239fdeb8a8', // календарь
         '654067e916584fb2bb3a091299b80315', // шар
-        'aba7a7eb4d15476f8c4c592069597411', // 
-        'b93482d68e3e4355aa9fc845aaabc9e8',
-        '1f5c757a61da444f9a5f47bbab11c5f7',
-        '1ec0a6051d0d4c03954fb3a57fb1ab3c',
-        '91fa7ba44b0b4a44b7889b862ea6cac0',
-        '5b9bdd73b1c341c7ab952bfe569ae4a3',
-        '2bc0346ef50f40a681c98becf2f2b060',
-        'b7911693d6cf467e8173d24045948aef'
+        'b7911693d6cf467e8173d24045948aef', // головоломка с цепочкой внутри рюкзака
     ];
     $routeProvider.when('/', {
         templateUrl : 'pages/home.html',
